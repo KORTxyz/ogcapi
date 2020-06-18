4225bb4746d7894bd65a7e3cacd7d1849a1900e3
Tino Kastbjerg Stigsen <tinoks@gmail.com>
2020-06-18 10:26:47 +0200

    console log on datadir

--- util/datadir.js
+++ util/datadir.js
@@ -9,8 +9,9 @@ const module_exists =  name => {
 
 const readFile = async file => {
   const extName = path.extname(file.basename).substr(1).toLowerCase()
+    console.log("datadir",file.basename, file.fullPath)
     if(module_exists('./fileTypes/'+extName)){
-
+      
       const item = await gdal.open(file.fullPath);
       const fileType = item.driver.description;
       console.log(fileType, file.basename)
