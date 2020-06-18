feb9ac68685b1ed06a7e942e4ac4e36f0e643706
Tino Kastbjerg Stigsen <tinoks@gmail.com>
2020-06-18 10:40:06 +0200

    rename fileTypes

--- util/datadir.js
+++ util/datadir.js
@@ -9,21 +9,21 @@ const module_exists =  name => {
 
 const readFile = async file => {
   const extName = path.extname(file.basename).substr(1).toLowerCase()
-    console.log("datadir",file.basename, file.fullPath)
-    if(module_exists('./fileTypes/'+extName)){
+    console.log("datadir",file.fullPath)
+    if(module_exists('./filetypes/'+extName)){
       
       const item = await gdal.open(file.fullPath);
       console.log(item)
       const fileType = item.driver.description;
       console.log(fileType, file.basename)
 
-      const fileReader = require('./fileTypes/'+fileType);
+      const fileReader = require('./filetypes/'+fileType);
 
       const metadata = await fileReader(file.fullPath)
       global.collectionDB.insert(metadata).catch(err=>{ throw err; })
     }
     else{
-      console.error("Can't find module",'./fileTypes/'+extName,". For file ",file.fullPath)
+      console.error("Can't find module",'./filetypes/'+extName,". For file",file.fullPath)
     }
 }
 
