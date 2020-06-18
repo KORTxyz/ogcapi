6b53a10c7d6afba585212e0839d29ac0a2f83713
Tino Kastbjerg Stigsen <tinoks@gmail.com>
2020-06-18 10:48:12 +0200

    modulePath

--- util/datadir.js
+++ util/datadir.js
@@ -10,20 +10,21 @@ const module_exists =  name => {
 const readFile = async file => {
   const extName = path.extname(file.basename).substr(1).toLowerCase()
     console.log("datadir",file.fullPath)
-    if(module_exists(path.resolve("filetypes", extName))){
-      
+    const modulePath = path.resolve("util/filetypes", extName.toLowerCase())
+    if(module_exists(modulePath)){
+      /*
       const item = await gdal.open(file.fullPath);
       console.log(item)
       const fileType = item.driver.description;
       console.log(fileType, file.basename)
-
-      const fileReader = require(path.resolve("filetypes", fileType));
+      */
+      const fileReader = require(modulePath);
 
       const metadata = await fileReader(file.fullPath)
       global.collectionDB.insert(metadata).catch(err=>{ throw err; })
     }
     else{
-      console.error("Can't find module",path.resolve("filetypes", extName),". For file",file.fullPath)
+      console.error("Can't find module",modulePath,". For file",file.fullPath)
     }
 }
 
